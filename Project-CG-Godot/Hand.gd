722b240eb22d7dd3f738f5c92f6dedf07afd8a42
Stoica Vlad Ionut <136201848+Rilotro@users.noreply.github.com>
2023-10-23 19:01:42 +0300

    Implemented a grid-like behaviour
    it's still a work in progress

--- Project-CG-Godot/Hand.gd
+++ Project-CG-Godot/Hand.gd
@@ -14,5 +14,16 @@ func _ready():
 func _process(delta):
 	if(Input.is_action_just_pressed("TestCards")):
 		var tempCard = Card.instantiate();
-		add_child(tempCard);
 		tempCard.add_child(CDB[rng.randi_range(0, 3)].instantiate());
+		$CardArea/CardGrid.add_child(tempCard);
+		match $CardArea/CardGrid.get_child_count():
+			2:
+				$CardArea/CardGrid/LeftGap.custom_minimum_size = Vector2(287.5, 0);
+			3:
+				$CardArea/CardGrid/LeftGap.custom_minimum_size = Vector2(219, 0);
+			4:
+				$CardArea/CardGrid/LeftGap.custom_minimum_size = Vector2(94.5, 0);
+			5:
+				$CardArea/CardGrid/LeftGap.custom_minimum_size = Vector2(26, 0);
+			_:
+				$CardArea/CardGrid/LeftGap.custom_minimum_size = Vector2(0, 0);
